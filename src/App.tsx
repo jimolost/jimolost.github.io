e48f16038c48c9511a5f2582570c8b65df0bb967
lebovsky <lebovsky@example.ru>
2023-12-07 00:44:47 +0300

    Четвёртый  коммит v2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+/* eslint react/no-unescaped-entities */
 
 function App() {
   return (
