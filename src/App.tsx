5e4512522661be0d86dbc6a4ea355b70f6c49115
lebovsky <lebovsky@example.ru>
2023-12-06 23:26:44 +0300

    Третий  коммит

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,39 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          <div class="container">
+
+            <h1>How to Install Bootstrap in React App - ItSolutionStuff.com</h1>
+
+
+
+            <button class="btn btn-success">Success Button!</button>
+
+            <button class="btn btn-primary">Primary Button!</button>
+
+            <button class="btn btn-danger">Danger Button!</button>
+
+
+
+            <div class="alert alert-success">
+
+              <p>This is success alert.</p>
+
+            </div>
+
+            <div class="alert alert-primary">
+
+              <p>This is primary alert.</p>
+
+            </div>
+
+            <div class="alert alert-danger">
+
+              <p>This is danger alert.</p>
+
+            </div>
+
+          </div>
         </p>
       </header>
     </div>
